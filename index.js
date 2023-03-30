f5d33437e5af241af34b0e638ca360ea847ba095
Kyaw Mg Mg Thu <mthu35997@gmail.com>
2023-03-30 15:39:48 +0630

    local commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// static number 
+// prompt();
+// parseInt
+let a =parseInt (prompt ("Enter your first character: "));
+let b =parseInt (prompt ("Enter your second character: "));
+// +
+var c = a + b;
+//The sum of a and b is answer 
+console.log ("The sum of "+a+" and "+b+" is "+c);
